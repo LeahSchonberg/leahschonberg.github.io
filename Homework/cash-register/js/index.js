66e8f493e9606f1403e950a46dda9a5ef8e74a59
Leah Schonberg <leahschonberg@gmail.com>
2015-12-14 21:29:54 -0500

    Register

--- Homework/cash-register/js/index.js
+++ Homework/cash-register/js/index.js
@@ -8,22 +8,24 @@
 // 2: Remeber to prevent the default action on the form!
 // 3: Create a variable to store the user input from #newEntry
 // 4: Remember to call parseFloat() on this variable! It needs to be a number.
-// 5: Append a new <tr></tr> to the <tbody id="entries"> containing two <td>'s, one of which contains the new variable: <tr><td></td><td>NEW VALUE</td></tr>
+// 5: Append a new <tr></tr> to the <tbody id="entries"> containing two <td>'s, one of which contains the new variable: 
 // 6: Figure out a way to update the number in #total (Hint: look back at the calculator box example)
 // 7: Empty the input in #newEntry (clear it out)
 
 $(document).ready(function() {
-    $('#entry').submit(function(event) {
-        event.preventDefault();
-        var total = $('#newEntry').val()
-        total = parseFloat(total)
-        $('#entries').append('<tr>+<td>+</td>+<td>' + total +
-            '</td></tr>');
-    })
-    total = total + entry;
-$('#newEntry').val('');
-
-
-
-    
+$('#entry').submit(function()
+}
+event.(preventDefault();
+var total = ('#newEntry');
+entry = parseFloat(entry);
+$('#entries').append('<tr><td></td><td>' + total + '</td></tr>');
 })
+
+function submit (total) {
+ 
+  var total = total + entry
+}
+
+
+
+
