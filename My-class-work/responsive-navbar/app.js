4b6afe1fe5311df388f783e735154cc2595f2710
Leah Schonberg <leahschonberg@gmail.com>
2015-12-22 18:24:08 -0500

    updates

---
+++ My-class-work/responsive-navbar/app.js
@@ -0,0 +1,13 @@
+$(document).ready(function() {
+	$('#icon').click(function(){
+		$('#small-nav').slideToggle()
+	})
+
+	$(window).resize(function (){
+		if ($(window).width() > 900px)	{
+
+		}
+
+	})
+
+})
